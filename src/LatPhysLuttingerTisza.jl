89d707dfa9e386989f77bde7ffc22cc42c90f0c6
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-12-14 12:29:46 +0100

    updated scope with comments

--- src/LatPhysLuttingerTisza.jl
+++ src/LatPhysLuttingerTisza.jl
@@ -1,3 +1,38 @@
+################################################################################
+#
+#   module LatPhysLuttingerTisza
+#   -> LatticePhysics_Base
+#   -> LatticePhysics_ReciprocalSpace
+#   -> LinearAlgebra
+#
+#   --> TYPE DEFINITIONS
+#           - LTBandstructure
+#           - LTGroundstateManifold
+#
+#   --> IO OF TYPES
+#
+#   --> SPIN INTERACTION MATRICES
+#
+#   --> CALCULATION OF LT CONSTRAINTS
+#
+#   --> CALCULATION OF LT BANDSTRUCTURES AND EIGENVALUES
+#           - in real space (pure eigenvalues)
+#           - in momentum space (along cardinal directions)
+#           - in momentum space (along reciprocal lattice directions)
+#           - in momentum space (along path)
+#
+#   --> CALCULATION OF LT GROUNDSTATE MANIFOLDS
+#           - calculation for 2D unitcells
+#           - calculation for 3D unitcells
+#
+################################################################################
+
+
+
+
+
+
+# start of module
 module LatPhysLuttingerTisza
 
 # used libraries
@@ -6,5 +41,5 @@ using LatPhysReciprocal
 using LinearAlgebra
 
 
-
-end # module
+# end of module
+end
