3c0aade21b158448abf4c711462bee7883ce49ab
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-12-17 10:32:32 +0100

    added constructor function to LT Hamiltonian

--- src/LT_hamiltonian_definition.jl
+++ src/LT_hamiltonian_definition.jl
@@ -4,8 +4,11 @@ struct LTHamiltonian{
         HB<:AbstractBondHamiltonian{L,NS} where {NS}
     }
 
+    # the unitcell
+    unitcell :: U
+
     # the bond Hamiltonian
-    h_bond :: HB
+    h_bond   :: HB
 
     # ...
 
@@ -13,8 +16,14 @@ end
 
 
 # function to create such an object
+function newLTHamiltonian(
+            unitcell :: U,
+            bond_hamiltonian :: HB
+        ) :: LTHamiltonian{L,U,HB} where {L,U<:AbstractUnitcell{S,B} where {S, B<:AbstractBond{L,N} where {N}},HB<:AbstractBondHamiltonian{L,NS} where {NS}}
 
-
+    # return such an object
+    return LTHamiltonian{L,U,HB}(unitcell, bond_hamiltonian)
+end
 
 # function to obtain the Hamiltonian matrix at some k
 function getHamiltonianAtK(
